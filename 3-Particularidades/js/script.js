b1b75dc405acb5d2fef76937faa61db1b28a151c
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2023-10-06 10:30:16 -0300

    Particularidades do JS

---
+++ 3-Particularidades/js/script.js
@@ -0,0 +1,17 @@
+// O jS Executa o código de cima para baixo 
+console.log("Primeiro");
+
+console.log("Segundo");
+
+/** O js é case sensitive nesse exemplo o Log com o l maiúsculo da erro.
+ * console.Log("Mais um console.log");
+ */
+
+
+
+/*tipagem fraca
+variável (a) declarada e iniciada com valor 1
+porem valor alterado com facilidadee por isso tipagem fraca */
+var a = 1;
+a = 'teste';
+console.log(a);
